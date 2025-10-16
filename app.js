4ec10d605661b74752b4b9df28423d5195af6881
ak_alb0 <andi-kelmendi@hotmail.com>
2025-10-16 11:57:39 +0200

    feat(index) commit

--- app.js
+++ app.js
@@ -27,3 +27,61 @@
     //     showSlide(index);
     // }, 8000);
 
+
+
+// partie recherche
+
+
+const searchInput = document.querySelector('.nav2 input[type="search"]');
+const searchOverlay = document.getElementById('searchOverlay');
+const closeSearch = document.getElementById('closeSearch');
+
+searchInput.addEventListener('focus', () => {
+  searchOverlay.style.display = 'flex';
+  searchOverlay.classList.add('show');
+  document.querySelector('.search-input-focus').focus();
+});
+
+closeSearch.addEventListener('click', () => {
+  searchOverlay.classList.remove('show');
+  setTimeout(() => {
+    searchOverlay.style.display = 'none';
+  }, 300); // doit correspondre à la durée de transition
+});
+
+
+
+// partie  menu burger
+
+
+const burger = document.getElementById('menu');
+const fullscreenMenu = document.createElement('div');
+fullscreenMenu.classList.add('fullscreen-menu');
+fullscreenMenu.innerHTML = `
+  <i class="ri-close-line" id="closeMenu"></i>
+  <ul>
+    <li><a href="#">New Arrivals</a></li>
+    <li><a href="#">Men</a></li>
+    <li><a href="#">Women</a></li>
+    <li><a href="#">Accessories</a></li>
+  </ul>
+`;
+document.body.appendChild(fullscreenMenu);
+
+burger.addEventListener('click', () => {
+  fullscreenMenu.style.display = 'flex';
+  setTimeout(() => {
+    fullscreenMenu.classList.add('show');
+  }, 10); // léger délai pour déclencher l'animation
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.id === 'closeMenu') {
+    fullscreenMenu.classList.remove('show');
+    setTimeout(() => {
+      fullscreenMenu.style.display = 'none';
+    }, 400); // durée de l'animation
+  }
+});
+
+
