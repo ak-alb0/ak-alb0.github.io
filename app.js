4f8dc7da407b3598a558333e452c499dae902fd8
ak_alb0 <andi-kelmendi@hotmail.com>
2025-11-05 10:08:15 +0100

    commit

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const userIcon = document.querySelector(".ri-user-3-line");
-  userIcon.addEventListener("click", () => {
-    window.location.href = "login.php";
-  });
-});
-
 
